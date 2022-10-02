35548a8b1d3002ef43cdd57bcd965a32d1f776d3
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-10-02 10:10:27 +0300

    added changes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import styles from './Modal.module.css';
 
-export default function Modal(image, tags, closeModal) {
-
+export default function Modal({ image, tags, closeModal }) {
+  
     useEffect(() => {
         const onEsc = (e) => {
             if (e.code === 'Escape') closeModal();
         };
         window.addEventListener('keydown', onEsc);
         return () => window.removeEventListener('keydown', onEsc)
-    });
+    }, [closeModal]);
 
      return (
             <div className={styles.Overlay}>
